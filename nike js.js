093664b26f3c66d7a113415d19af7e1fe6fcab65
Alexandar <100527128+JustNewbieDev@users.noreply.github.com>
2022-03-27 22:25:17 +0300

    Updated a new sticky responsive filter menu
    Made a new responsive sticky filter menu idecntical to the one in Nike's site, with the checkboxes and everything!

--- nike js.js
+++ nike js.js
@@ -1,9 +1,10 @@
 const navbar = document.querySelector(".navbar4");
+const navbar2 = document.querySelector(".menu4");
 
 window.onscroll = ()=>{
 	this.scrollY > 230 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");;
+	this.scrollY > 230 ? navbar2.classList.add("sticky2") : navbar2.classList.remove("sticky2");;
 }
-
 
 
 
